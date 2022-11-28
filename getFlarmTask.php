63d1790710d5e5eecc1aeae727f3a3bea94bf0c2
brunotag <bruno.tagliapietra@gmail.com>
2022-11-28 22:17:00 +1300

    Fix tracking
    List of broken things
    
    1) the "glidingops.com" url hardcoded in about 50 places
    2) gliding.net.nz doesn't reply to http requests anymore, only https
    3) a pre-existent bug, in GOPs, preventing it from reading from
    gliding.net.nz (it was looking for a "lon" longitude field, whilst
    gliding.net.nz calls it "lng" - meaning the integration with
    gliding.net.nz never worked, only the "live" one with
    "live.glidernet.org" was)
    
    Now the "interesting" thing is that fixing (3) might cause undesired
    effects ... we'll see :-)

--- getFlarmTask.php
+++ getFlarmTask.php
@@ -9,117 +9,104 @@ require 'ognClass.php';
 require 'GlidingGNZClass.php';
 require dirname(__FILE__) . '/includes/classGlidingDB.php';
 
-$con_params = require( dirname(__FILE__) .'/config/database.php');
-$con_params = $con_params['gliding'];
-$DB = new GlidingDB($con_params);
+try {
 
-//Curret time
-$dt = new DateTime();
+    $con_params = require(dirname(__FILE__) . '/config/database.php');
+    $con_params = $con_params['gliding'];
+    $DB = new GlidingDB($con_params);
 
-//Find out who is flying today
-//TODO: replace absolute url
-$myGlide = new Gliding('gops.wwgc.co.nz');
-//TODO: replace hardcoded org (1)
-$flyingToday = $myGlide->getFlyingToday(1);
+    //Curret time
+    $dt = new DateTime();
 
-//Build a list of gliders
-$gliderlist = array();
+    //Find out who is flying today
+    //TODO: replace absolute url
+    $myGlide = new Gliding('gops.wwgc.co.nz');
+    //TODO: replace hardcoded org (1)
+    $flyingToday = $myGlide->getFlyingToday(1);
 
-$flyNow = $flyingToday['data'] ['flying'];
-$flyDone = $flyingToday['data'] ['completed'];
-foreach ($flyNow as $f)
-{
-    if (strlen($f['glider']) > 0)
-    {
-        $g = strtoupper($f['glider']);
-        if (!in_array($g,$gliderlist) )
-        {
-            array_push($gliderlist,$g);
-        }
-    }
-}
+    //Build a list of gliders
+    $gliderlist = array();
 
-foreach ($flyDone as $f)
-{
-    if (strlen($f['glider']) > 0)
-    {
-        $g = strtoupper($f['glider']);
-        if (!in_array($g,$gliderlist) )
-        {
-            array_push($gliderlist,$g);
-        }
-    }
-}
-
-//Build a table of flarm codes
-$flarmCode = array();
-
-if (count($gliderlist) > 0)
-{
-    foreach ($gliderlist as $gld)
-    {
-        $r = $myGlide->getFlarmCode($gld);
-        if ($r['meta'] ['status'] == "OK")
-        {
-            $ICAO = $r['data'] ['flarmcode'];
-            if (strlen($ICAO) > 0)
-            {
-                $flarmCode[$gld] = $ICAO;
+    $flyNow = $flyingToday['data']['flying'];
+    $flyDone = $flyingToday['data']['completed'];
+    foreach ($flyNow as $f) {
+        if (strlen($f['glider']) > 0) {
+            $g = strtoupper($f['glider']);
+            if (!in_array($g, $gliderlist)) {
+                array_push($gliderlist, $g);
             }
         }
     }
 
-    //Now source data for OGN
-    $myOGN = new ogn();
-    $rogn =  json_decode($myOGN->getCurrentFlarms(),true);
-    //TODO: replace absolute url
-    $myGNZ = new GNZ('www.gliding.net.nz');
+    foreach ($flyDone as $f) {
+        if (strlen($f['glider']) > 0) {
+            $g = strtoupper($f['glider']);
+            if (!in_array($g, $gliderlist)) {
+                array_push($gliderlist, $g);
+            }
+        }
+    }
 
-    $dtNZ = new DateTime();
-    $dtNZ->setTimezone(new DateTimeZone('Pacific/Auckland'));
-    $dayStr = $dtNZ->format('Y-m-d');
+    //Build a table of flarm codes
+    $flarmCode = array();
 
-    foreach ($gliderlist as $gld)
-    {
-        $ICAO = '';
-        if (array_key_exists($gld,$flarmCode))
-            $ICAO = $flarmCode[$gld];
-        if (strlen($ICAO) > 0)
-        {
-            echo "Have flarm code for " . $gld . " of ". $ICAO ." \n";
-            if (array_key_exists($ICAO,$rogn) )
-            {
-                $p = $rogn[$ICAO];
-                /*
+    if (count($gliderlist) > 0) {
+        foreach ($gliderlist as $gld) {
+            $r = $myGlide->getFlarmCode($gld);
+            if ($r['meta']['status'] == "OK") {
+                $ICAO = $r['data']['flarmcode'];
+                if (strlen($ICAO) > 0) {
+                    $flarmCode[$gld] = $ICAO;
+                }
+            }
+        }
+
+        //Now source data for OGN
+        $myOGN = new ogn();
+        $rogn =  json_decode($myOGN->getCurrentFlarms(), true);
+        //TODO: replace absolute url
+        $myGNZ = new GNZ('www.gliding.net.nz');
+
+        $dtNZ = new DateTime();
+        $dtNZ->setTimezone(new DateTimeZone('Pacific/Auckland'));
+        $dayStr = $dtNZ->format('Y-m-d');
+
+        foreach ($gliderlist as $gld) {
+            $ICAO = '';
+            if (array_key_exists($gld, $flarmCode))
+                $ICAO = $flarmCode[$gld];
+            if (strlen($ICAO) > 0) {
+                echo "Have flarm code for " . $gld . " of " . $ICAO . " \n";
+                if (array_key_exists($ICAO, $rogn)) {
+                    $p = $rogn[$ICAO];
+                    /*
                     As we can get this data later than its occurence, and we only get a time and no date
                     It is possibe, if we assume that its todays date, that it is in fact captured yeterday UTC.
                     So if the timestanp is more than say 10 minutes ahead of now, we can assume it was the day before.
                     The 10 minute window allows for clock sync issues.
                 */
-                $dtgps = new DateTime($dt->format('Y-m-d') . " " . $p['time']);
-                if ($dtgps->getTimestamp() > ($dt->getTimestamp() + 3600*12))
-                    $dtgps->setTimestamp($dtgps->getTimestamp() - 86400);
-                $strTime =  $dtgps->format('Y-m-d H:i:s');
-                $DB->createTrack(1,$gld,$strTime,0,$p['lat'],$p['lon'],$p['alt'],'FlarmOGN');
-            }
-
-            //Now check GNZ for same
-            try
-            {
-                $gnzData = $myGNZ->getFlarmData($dayStr,$ICAO);
-                $data = $gnzData['data'];
-                foreach ($data as $r)
-                {
-                    $DB->createTrack(1,$gld,$r['thetime'],0,$r['lat'],$r['lon'],$r['alt'],'FlarmGNZ');
+                    $dtgps = new DateTime($dt->format('Y-m-d') . " " . $p['time']);
+                    if ($dtgps->getTimestamp() > ($dt->getTimestamp() + 3600 * 12))
+                        $dtgps->setTimestamp($dtgps->getTimestamp() - 86400);
+                    $strTime =  $dtgps->format('Y-m-d H:i:s');
+                    $DB->createTrack(1, $gld, $strTime, 0, $p['lat'], $p['lon'], $p['alt'], 'FlarmOGN');
                 }
 
+                //Now check GNZ for same
+                try {
+                    //example: https://www.gliding.net.nz/api/v1/tracking/2022-11-28/C81B5D/pings
+                    $gnzData = $myGNZ->getFlarmData($dayStr, $ICAO);
+                    $data = $gnzData['data'];
+                    foreach ($data as $r) {
+                        //TODO: replace hardcoded org (1)
+                        $DB->createTrack(1, $gld, $r['thetime'], 0, $r['lat'], $r['lng'], $r['alt'], 'FlarmGNZ');
+                    }
+                } catch (exception $e) {
+                    echo "Exception getting GNZ Data\n" . $e;
+                }
             }
-            catch (exception $e)
-            {
-                echo "Exception getting GNZ Data\n";
-            }
-
         }
     }
+} catch (exception $e) {
+    echo "Exception running getFlarmTask\n" . $e;
 }
-?>
