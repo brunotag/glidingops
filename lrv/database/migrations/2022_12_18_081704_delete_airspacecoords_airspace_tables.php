972c3dfc7198d7c21693a29bcc2295022a2d5e4d
brunotag <bruno.tagliapietra@gmail.com>
2022-12-18 21:25:40 +1300

    delete airspace *** tables

---
+++ lrv/database/migrations/2022_12_18_081704_delete_airspacecoords_airspace_tables.php
@@ -0,0 +1,56 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class DeleteAirspacecoordsAirspaceTables extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('airspacecoords', function (Blueprint $table) {
+            $table->dropForeign('airspacecoords_ibfk_1');
+        });
+        Schema::drop('airspacecoords');
+        Schema::drop('airspace');
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::create('airspace', function (Blueprint $table) {
+            $table->integer('id', true);
+            $table->string('name', 20)->nullable();
+            $table->string('region', 20)->nullable();
+            $table->string('type', 10)->nullable();
+            $table->string('class', 10)->nullable();
+            $table->integer('upper_height')->nullable();
+            $table->integer('Lower_height')->nullable();
+        });
+
+        Schema::create('airspacecoords', function (Blueprint $table) {
+            $table->integer('id', true);
+            $table->integer('airspace')->nullable()->index('idx_airspace');
+            $table->integer('seq')->nullable();
+            $table->string('type', 6)->nullable();
+            $table->float('lattitude', 10, 0)->nullable();
+            $table->float('longitude', 10, 0)->nullable();
+            $table->float('arclat', 10, 0)->nullable();
+            $table->float('arclon', 10, 0)->nullable();
+            $table->float('arcdist', 10, 0)->nullable();
+        });
+
+        Schema::table('airspacecoords', function (Blueprint $table) {
+            $table->foreign('airspace', 'airspacecoords_ibfk_1')->references('id')->on('airspace')->onUpdate('NO ACTION')->onDelete('NO ACTION');
+        });
+    }
+}
