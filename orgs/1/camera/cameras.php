f0f56cc63d9eb8165c396be0bd86f6036132ec50
Bruno Tagliapietra <bruno.tagliapietra@gmail.com>
2024-03-12 22:32:04 +1300

    add camera5

--- orgs/1/camera/cameras.php
+++ orgs/1/camera/cameras.php
@@ -1,3 +1,3 @@
 <?php   
-  $camera_names = isset($_GET["camera-names"]) ? $_GET["camera-names"] : array("camera2","camera3","camera4");
+  $camera_names = isset($_GET["camera-names"]) ? $_GET["camera-names"] : array("camera2","camera3","camera4","camera5");
 ?>
