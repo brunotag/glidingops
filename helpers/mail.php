1b3b3af956cff8690b5e8314c2b44c597b5e57db
Bruno Tagliapietra <bruno.tagliapietra@gmail.com>
2024-08-27 23:11:53 +1200

    new email addresses

--- helpers/mail.php
+++ helpers/mail.php
@@ -4,7 +4,7 @@ class Mail
 {
     private static function getCommonHeaders($reply_to) {
         return 
-            'From: Gliding Operations <gops.wwgc.co.nz@gmail.com>' . "\r\n" .
+            'From: WWGC Gliding Operations <machinery.gops@wwgc.co.nz>' . "\r\n" .
             'Reply-To: ' . $reply_to . "\r\n" .
             'Return-PATH: gops.wwgc.co.nz@gmail.com' . "\r\n" .
             'X-Mailer: PHP/' . phpversion();
@@ -14,27 +14,27 @@ class Mail
     {
         //TODO: replace hardcoded domains
         $headers =  Mail::getCommonHeaders($reply_to) . "\r\n" .'Content-type: ' . $content_type;
-        return mail($to, $subject, $message, $headers, '-r gops.wwgc.co.nz@gmail.com');
+        return mail($to, $subject, $message, $headers, '-r machinery.gops@wwgc.co.nz');
     }    
 
     public static function SendMailPlainText($to, $subject, $message)
     {
-        return Mail::SendMail($to, $subject, $message, 'wgcoperations@gmail.com', 'text/plain');
+        return Mail::SendMail($to, $subject, $message, 'servicedelivery@wwgc.co.nz', 'text/plain');
     }
     
     public static function SendMailHtml($to, $subject, $message)
     {
-        return Mail::SendMail($to, $subject, $message, 'wgcoperations@gmail.com', 'text/html; charset=iso-8859-1');
+        return Mail::SendMail($to, $subject, $message, 'servicedelivery@wwgc.co.nz', 'text/html; charset=iso-8859-1');
     }
 
     public static function SendMailPlainTextReplyTo($to, $subject, $message, $reply_to)
     {
-        return Mail::SendMail($to, $subject, $message, 'wgcoperations@gmail.com, '. $reply_to, 'text/plain');
+        return Mail::SendMail($to, $subject, $message, 'servicedelivery@wwgc.co.nz, '. $reply_to, 'text/plain');
     }
     
     public static function SendMailHtmlReplyTo($to, $subject, $message, $reply_to)
     {
-        return Mail::SendMail($to, $subject, $message,'wgcoperations@gmail.com, '. $reply_to, 'text/html; charset=iso-8859-1');
+        return Mail::SendMail($to, $subject, $message,'servicedelivery@wwgc.co.nz, '. $reply_to, 'text/html; charset=iso-8859-1');
     }
     
 
