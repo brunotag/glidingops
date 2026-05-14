f02ea8e8bce3123ec1034003795ca753929bfe23
Bruno Tagliapietra <bruno.tagliapietra@gmail.com>
2026-05-15 02:46:06 +1200

    feat(dev): start-dev also vagrant up

--- start-dev.ps1
+++ start-dev.ps1
@@ -1,6 +1,8 @@
 $AGENTMEMORY_DIR = Join-Path $PSScriptRoot ".agentmemory"
 $PROJ = $PWD.Path
+$LARAVEL = Join-Path $PSScriptRoot "lrv"
 
 wt -w 0 nt -d $AGENTMEMORY_DIR --title "iii-engine" --tabColor "#44a39b" powershell -NoExit -Command "./run-cmd.cmd"
 wt -w 0 nt -d $AGENTMEMORY_DIR --title "agentmemory" --tabColor "#f3cd21" powershell -NoExit -Command "npx -y @agentmemory/agentmemory"
 wt -w 0 nt -d $PROJ --title "opencode" --tabColor "#90137b" powershell -NoExit -Command "opencode"
+wt -w 0 nt -d $LARAVEL --title "vagrant" --tabColor "#201390" powershell -NoExit -Command "vagrant up"
